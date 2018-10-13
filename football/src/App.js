6a2a88090c2cdc462f08b5edecd7181f06f3a9d8
jonnyps <jonny_spry@msn.com>
2018-10-13 20:06:06 +0100

    add CORS proxy to avoid using header authentication

--- football/src/App.js
+++ football/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// import { Cookies } from 'react-cookie';
+// Cookies.set(token, '9d006876cd4d43f08084a828529fc968', {path: '/'});
 
 class App extends Component {
   
@@ -10,16 +12,20 @@ class App extends Component {
   }
 
   componentDidMount() {
-
-    fetch("http://api.football-data.org/v2/competitions")
-    // fetch("https://randomuser.me/api/?results=500")
-    .then(results => {
-      return results.json();
-    }).then(data => {
-      console.log(data)
-
-    })
-  }
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "http://api.football-data.org/v2/competitions"; // site that doesn’t send Access-Control-*
+    fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+    .then(response => response.text())
+    .then(contents => console.log(contents))
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+    // fetch("http://api.football-data.org/v2/competitions")
+    // // fetch("https://randomuser.me/api/?results=500")
+    // .then(results => {
+    //   return results.json();
+    // }).then(data => {
+    //   console.log(data)
+    // })
+}
 
   render() {
     return (

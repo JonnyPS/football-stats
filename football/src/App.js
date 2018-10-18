84f5a29fc52fdc8420c8341e2bd1ab9d5642b8ab
Jonny Spry <jspry@energyinst.org>
2018-10-18 18:03:50 +0100

    final commit of day, error being produced trying to render data obj

--- football/src/App.js
+++ football/src/App.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 // import { Cookies } from 'react-cookie';
 // Cookies.set(token, '9d006876cd4d43f08084a828529fc968', {path: '/'});
 
+function TeamList (props) {
+  // return null
+  // console.log(footballData)
+  return (
+    <ul>
+      {props.list.map((name) => (
+        <li key={name}>
+          <span>{name}</span>
+        </li>
+      ))}
+    </ul>
+  )
+  return props.list.length;
+}
+
 class App extends Component {
   
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      competitions: [],
+      data: null,
+      // teams: ['arsenalTeam', 'chelseaTeam', 'somethingTeam'],
+      // getTeams: () => { jsonData.map((name) => {teams.push(name)} },
     }
+    // this.component = this.component.bind(this)
   }
 
   componentDidMount() {
-    const  uri = 'http://api.football-data.org/v2/competitions'; 
+    const uri = 'http://api.football-data.org/v2/competitions/PL/teams'; 
 
     let h = new Headers()
     h.append('Accept', 'application/json')
@@ -23,49 +41,46 @@ class App extends Component {
       mode: 'cors' 
     })
 
-    fetch( req )  
-      .then( (response) => {
-        if ( response.ok ) {
-          return response.json();
-        } else {
-          throw new Error( 'Bad http request or something similar' );
-        }
-       })
-      .then( (jsonData) => {
-        console.log( jsonData )
-       })
-      .catch( (err) => {
-        console.log('ERROR:', err.message)
-      }) 
+    var component = this;
+
+    fetch(req)
+     .then( (response) => {
+        return response.json()    
+     })
+     .then( (json) => {
+        component.setState({
+           data: json
+        })
+        console.log('parsed json', json)
+     })
+     .catch( (ex) => {
+        console.log('parsing failed', ex)
+     })
+     console.log(this.state.data)
+  }
 
 
-    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    // const url = "https://randomuser.me/api/?results=500"; // site that doesn’t send Access-Control-*
-    // fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-    // // console.log('fetching...')
-    // .then(results => {
-    //   return results.json();
-    // }).then(data => {
-    //   console.log(data)
-    // })
-
-
-    // .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-    // fetch("http://api.football-data.org/v2/competitions")
-    // // fetch("https://randomuser.me/api/?results=500")
-    // .then(results => {
-    //   return results.json();
-    // }).then(data => {
-    //   console.log(data)
-    // })
-}
+  // setTeams(jsonData) {
+  //   // console.log( typeof jsonData )
+  //   console.log( jsonData.teams[0].className )
+  //   jsonData.teams.map((name) => {
+      
+  //     // this.setState(() => {
+  //     // return {  
+  //     //   teams: name
+  //     //   }
+  //     // })
+  //   })
+  // }
 
   render() {
     return (
-      <div className="App">
-        <h2>hello everyone</h2>
-        <h2>somthing new</h2>
+      <div>
+        <div className="App">
+        <TeamList list={this.state.data} />
       </div>
+
+      // </div>
     );
   }
 }

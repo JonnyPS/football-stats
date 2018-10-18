17a608a45bdba6268b414bdf7cf0176079998bb7
Jonny Spry <jspry@energyinst.org>
2018-10-18 16:34:08 +0100

    accessed api with auth headers and api token

--- football/src/App.js
+++ football/src/App.js
@@ -12,15 +12,45 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "http://api.football-data.org/v2/competitions"; // site that doesn’t send Access-Control-*
-    fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-    .then(results => {
-      return results.json();
-    }).then(data => {
-      console.log(data)
+    const  uri = 'http://api.football-data.org/v2/competitions'; 
+
+    let h = new Headers()
+    h.append('Accept', 'application/json')
+    h.append('X-Auth-Token', '9d006876cd4d43f08084a828529fc968')
+    let req = new Request(uri, {
+      method: 'GET',
+      headers: h,
+      mode: 'cors' 
     })
-    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+    fetch( req )  
+      .then( (response) => {
+        if ( response.ok ) {
+          return response.json();
+        } else {
+          throw new Error( 'Bad http request or something similar' );
+        }
+       })
+      .then( (jsonData) => {
+        console.log( jsonData )
+       })
+      .catch( (err) => {
+        console.log('ERROR:', err.message)
+      }) 
+
+
+    // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    // const url = "https://randomuser.me/api/?results=500"; // site that doesn’t send Access-Control-*
+    // fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+    // // console.log('fetching...')
+    // .then(results => {
+    //   return results.json();
+    // }).then(data => {
+    //   console.log(data)
+    // })
+
+
+    // .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
     // fetch("http://api.football-data.org/v2/competitions")
     // // fetch("https://randomuser.me/api/?results=500")
     // .then(results => {
@@ -34,6 +64,7 @@ class App extends Component {
     return (
       <div className="App">
         <h2>hello everyone</h2>
+        <h2>somthing new</h2>
       </div>
     );
   }

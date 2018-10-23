0460c1d70fa043839c223933b17961f5693bd9aa
Jonny Spry <jspry@energyinst.org>
2018-10-23 14:23:27 +0100

    bind updateInput and findMatches to the App component, crawl TeamDetails array for match on submit

--- football/src/App.js
+++ football/src/App.js
@@ -27,13 +27,14 @@ return null
 }
 
 function TeamInputField (props) {
-  console.log( props )
-  return (
-    <form>
-      <input type="text" />
-      <input type="submit" value="submit" />
-    </form>
-  )
+  return null;
+  // console.log( props )
+  // return (
+  //   <form>
+  //     <input type="text" />
+  //     <input type="submit" value="submit" />
+  //   </form>
+  // )
 }
 
 class App extends Component {
@@ -44,13 +45,18 @@ class App extends Component {
       allTeams: null,
       allMatches: null,
       teamDetails: [
-      {
-        a: 'Team Id',
-        b: 'Team Name',
-      },
+        {
+          a: 'Team Id',
+          b: 'Team Name',
+        },
       ],
       input: '',
     }
+
+    // because of where these functions are called... (?)
+    // we need to define that 'this', refers to our component App
+    this.updateInput = this.updateInput.bind( this )
+    this.findMatches = this.findMatches.bind( this )
   }
 
   componentDidMount() {
@@ -88,13 +94,12 @@ class App extends Component {
               return {
                 allTeams: json,
                 teamDetails: currentState.teamDetails.concat([{
-                  a: teamName,
-                  b: teamId
+                  teamName: teamName,
+                  teamId: teamId
                 }]),
               }
             })
           })
-
         } else {
           // when looping over urls[1], assign value to this.state.allMatches property
           this.setState(( currentState ) => {
@@ -103,11 +108,11 @@ class App extends Component {
             }
           })
         }
-        
         // once the data has loaded, 
         if ( this.state.allTeams !== null && this.state.allMatches !== null ) {
           console.log( 'teams:', this.state.allTeams )
           console.log( 'matches:', this.state.allMatches )        
+          console.log( 'teamDetails:', this.state.teamDetails )        
         }
         resourceCounter++
       })
@@ -115,19 +120,29 @@ class App extends Component {
         console.log('parsing failed:', ex)
       })
     })
+  }
 
-   function updateInput(e) {
-      const value = e.target.value
-      this.setState({
-        input: value
-      })
-      console.log( this.state.input )
+  updateInput(e) {
+    const value = e.target.value
+    this.setState({
+      input: value
+    })
+    console.log( this.state.input )
+  }
+
+  findMatches() {
+    console.log( 'findMatches' )
+    console.log( this.state )
+    console.log( 'team deets', this.state.teamDetails )
+    // get id of team from value of input
+    for (let item of this.state.teamDetails) {
+      if ( this.state.input == item.teamName ) {
+        console.log(item.teamName); // Will display contents of the object inside the array
+      }
     }
-
-    function findMatches() {
-      console.log( 'findMatches' )
-    }
-
+    // this.state.teamDetails[0].a.map(( id ) => { console.log( id ) } )
+    // use that id to look at matches object and pull out any matches where the home/away team has that id
+    // push those matches in to their own array
   }
 
   render(json) {
@@ -140,9 +155,8 @@ class App extends Component {
           value={this.state.input}
           onChange={this.updateInput}
         />
-        <button onClick={this.findMatches}>
-              Submit
-            </button>
+        <button onClick={this.findMatches}>Submit</button>
+        
       </div>
     )
     // const teamNum = 20

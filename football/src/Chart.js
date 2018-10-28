4ef3178a9d78bef411fe7f390e8fbce5e1507aa2
jonnyps <jonny_spry@msn.com>
2018-10-28 18:59:43 +0000

    add another component in another file to see if it renders

---
+++ football/src/Chart.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+function List (props) {
+  return (
+    <ul>
+    	{props.items.map( (value) => (
+    		<li key={value}>
+    			{value}
+			</li>
+    	))}
+    </ul>
+  )
+}
+
+class Chart extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+	  list: ['something', 'someone', 'somewhere'],
+	}
+  }
+
+  componentDidMount() {
+  	
+  }
+
+  render() {
+    return (
+      <List items={this.state.list} />
+    )
+  }
+}
+
+export default Chart;

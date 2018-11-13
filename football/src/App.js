13e4b7eefac6e2f0dfc01b860202af15891700c1
Jonny Spry <jonny_spry@msn.com>
2018-11-13 18:23:24 +0000

    add function to remove dataset from graph

--- football/src/App.js
+++ football/src/App.js
@@ -35,84 +35,102 @@ class App extends Component {
         {
           logo: 'img/arsenal.png',
           name: 'Arsenal',
-          colours: 'red',
+          colour: '#f00005',
         },
         {
           logo: 'img/bournemouth.png',
           name: 'Bournemouth',
-          colours: 'black',
+          colour: '#8d0104',
         },
         {
           logo: 'img/brighton-hove.png',
           name: 'Brighton Hove',
+          colour: '#005daa',
         },
         {
           logo: 'img/burnley.png',
           name: 'Burnley',
+          colour: '#fff515',
         },
         {
           logo: 'img/cardiff.png',
           name: 'Cardiff',
+          colour: '#035da9',
         },
         {
           logo: 'img/chelsea.png',
           name: 'Chelsea',
+          colour: '#024595',
         },
         {
           logo: 'img/crystal-palace.png',
           name: 'Crystal Palace',
+          colour: '#b80007',
         },
         {
           logo: 'img/everton.png',
           name: 'Everton',
+          colour: '#133e6c',
         },
         {
           logo: 'img/Fulham.png',
           name: 'Fulham',
+          colour: '#000000',
         },
         {
           logo: 'img/huddersfield.png',
           name: 'Huddersfield',
+          colour: '#003d93',
         },
         {
           logo: 'img/leicester-city.png',
           name: 'Leicester City',
+          colour: '#003d93',
         },
         {
           logo: 'img/liverpool.png',
           name: 'Liverpool',
+          colour: '#d00623',
         },
         {
           logo: 'img/man-city.png',
           name: 'Man City',
+          colour: '#5ba0da',
         },
         {
           logo: 'img/man-united.png',
           name: 'Man United',
+          colour: '#da030e',
         },
         {
           logo: 'img/newcastle.png',
           name: 'Newcastle',
+          colour: '#000',
         },
         {
           logo: 'img/southampton.png',
           name: 'Southampton',
+          colour: '#ed1a3b',
         },
         {
           logo: 'img/tottenham.png',
           name: 'Tottenham',
+          colour: '#aeaeae',
         },
         {
           logo: 'img/watford.png',
           name: 'Watford',
+          colour: '#fef823',
         },
         {
           logo: 'img/west-ham.png',
           name: 'West Ham',
+          colour: '#',
         },
         {
           logo: 'img/wolverhampton.png',
           name: 'Wolverhampton',
+          colour: '#faa61a',
         },        
       ],
 
@@ -158,6 +176,7 @@ class App extends Component {
     this.findMatches = this.findMatches.bind( this )
     this.selectTeam = this.selectTeam.bind( this )
     this.resetState = this.resetState.bind( this )
+    this.removeDataset = this.removeDataset.bind( this )
   }
 
   saveInitialState() {
@@ -168,6 +187,11 @@ class App extends Component {
   resetState() {
     console.log('resetState')
     this.setState( this.initialState[0] )
+  }
+
+  removeDataset() {
+    console.log('removeDataset')
+    this.setState( this.state.data.datasets.splice(-1, 1) )
   }
 
   selectTeam(name) {
@@ -268,10 +292,10 @@ class App extends Component {
       if ( this.state.input === item.teamName ) {
         console.log( 'item.teamName', item.teamName )
         var teamColour = this.state.profile.filter( (key) => { return key.name === item.teamName } )
-        var colours = teamColour[0].colours
+        var colour = teamColour[0].colour
 
         console.log( 'teamColour', teamColour) 
-        console.log( 'colours', colours) 
+        console.log( 'colour', colour) 
         console.log('teamDetails', this.state.teamDetails)
         let selectedTeamId = item.teamId
         let selectedTeamName = item.teamId
@@ -336,7 +360,7 @@ class App extends Component {
                   // borderColor: 'rgb(255, 99, 132)',
                   data: pointsSoFar,
                   // fixtures: fixtures,
-                  borderColor: colours,
+                  borderColor: colour,
                   backgroundColor: 'transparent',
                 })
               },
@@ -379,14 +403,14 @@ class App extends Component {
           width={400}
           options={{
             tooltips: {
-              callbacks: {
-                title: function( tooltipItem, data ) {
-                  return  data.datasets[0].fixtures[tooltipItem[0]['index']].home + ' v ' + data.datasets[0].fixtures[tooltipItem[0]['index']].away
-                },
-                afterTitle: function( tooltipItem, data ) {
-                  return  data.datasets[0].fixtures[tooltipItem[0]['index']].score
-                }
-              }
+              // callbacks: {
+              //   title: function( tooltipItem, data ) {
+              //     return  data.datasets[0].fixtures[tooltipItem[0]['index']].home + ' v ' + data.datasets[0].fixtures[tooltipItem[0]['index']].away
+              //   },
+              //   afterTitle: function( tooltipItem, data ) {
+              //     return  data.datasets[0].fixtures[tooltipItem[0]['index']].score
+              //   }
+              // }
             },
             legend: {
               display: true,
@@ -408,6 +432,7 @@ class App extends Component {
         />
 
         <button onClick={this.resetState}>Reset</button>
+        <button onClick={this.removeDataset}>Remove</button>
       </div>
     )
   }

dd7e91143c4a3552a85396c3807f0e47c677d533
JonnyPS <jonny_spry@msn.com>
2019-07-09 16:28:26 +0100

    add build dir via upload

---
+++ build/precache-manifest.0b40739326e2fac6afd5d042904d213e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f05d947a7e55ae437d45",
+    "url": "/static/js/main.f05d947a.chunk.js"
+  },
+  {
+    "revision": "d535d13dc4994bfae416",
+    "url": "/static/js/1.d535d13d.chunk.js"
+  },
+  {
+    "revision": "f05d947a7e55ae437d45",
+    "url": "/static/css/main.91ecb927.chunk.css"
+  },
+  {
+    "revision": "bfa234656d626a58527c4a40f35cc8f1",
+    "url": "/index.html"
+  }
+];

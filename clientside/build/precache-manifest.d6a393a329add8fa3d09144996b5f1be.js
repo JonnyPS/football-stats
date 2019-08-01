ec816b3a0cef1e70af0e9062c97ea31ef4f4fb0d
Jonny Spry <jspry@energyinst.org>
2019-08-01 12:21:18 +0100

    change location of .gitignore, make sure it ignores node_modules in sub directories

---
+++ clientside/build/precache-manifest.d6a393a329add8fa3d09144996b5f1be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "690c3289d41ec912a4d3",
+    "url": "/static/js/main.690c3289.chunk.js"
+  },
+  {
+    "revision": "d535d13dc4994bfae416",
+    "url": "/static/js/1.d535d13d.chunk.js"
+  },
+  {
+    "revision": "690c3289d41ec912a4d3",
+    "url": "/static/css/main.353c6f25.chunk.css"
+  },
+  {
+    "revision": "48b68d5ba2e2d2ecd3cafe509795523b",
+    "url": "/index.html"
+  }
+];
